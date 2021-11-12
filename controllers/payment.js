77b6d9c42ae74883383cdc87f5d6c9bfd2c91bb5
jassy1995 <babatundejoseph85@gmail.com>
2021-11-12 16:31:31 +0100

    i change response from array to string

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,40 +1,36 @@
-const UserDetail = require("../models/user");
 const axios = require("axios");
 let step = 1;
 let amount;
 let phone;
+let myFunc;
 exports.PayMe = async (req, res, next) => {
   const { message, phone_number } = req.body;
-  let response;
   if (
-    message.toLowerCase() === "payment" &&
+    message?.toLowerCase() === "payment" &&
     /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g.test(phone_number) &&
     step === 1
   ) {
     phone = phone_number;
-    response =
-      "Welcome! This service allows to fulfil a payment to a merchant.";
     try {
       const userRequest = await axios.post(
         "https://sellbackend.creditclan.com/parent/index.php/globalrequest/get_payment__order",
         { phone: phone_number }
       );
-      if (userRequest.data.message === "No record found") {
+      if (!userRequest.status) {
         return res.status(200).json({
           message: "There are no request payment for this number",
-          status: "success",
         });
-      } else {
+      } else if (userRequest.status) {
         let user_phone = userRequest.data.order.find(({ phone }) => phone);
         amount = user_phone.amount;
         step = 2;
         return res.status(200).json({
-          message: response,
-          notes: "We have found the following payment request",
-          data: userRequest.data.order,
-          response: "kindly enter 1 or 2 to select your option",
-          option: ["To Make Payment", "Decline"],
+          message: `Welcome! This service allows to fulfil a payment to a merchant. \n We have found  payment request for you. \n *amount*: ${userRequest.data.order[0].amount} \n *merchant*: ${userRequest.data.order[0].merchantName} \n *desc*: ${userRequest.data.order[0].description} \n  \n kindly enter  *[1]* To Make Payment \n *[2]* To Decline`,
         });
+      } else {
+        return res
+          .status(500)
+          .json({ message: "error occur,please try again" });
       }
     } catch (error) {
       return res.status(500).json(error);
@@ -53,22 +49,21 @@ exports.PayMe = async (req, res, next) => {
         );
         if (result) {
           step = 3;
-          response = `Kindly make a payment of ${result.data.data.amount} to the ${result.data.data.account_number} account number,${result.data.data.bank_name}`;
           return res.status(200).json({
-            message: response,
-            status: "success",
-            response: "kindly enter 1 to confirm your payment",
+            message: `Kindly make a payment of ${result.data.data.amount} to the account below \n *account No* : ${result.data.data.account_number} \n *bank* : ${result.data.data.bank_name} \n \n kindly enter *[1]* to confirm your payment`,
           });
         }
       } catch (error) {
-        return res.status(500).json({ message: error, status: "failed" });
+        return res.status(500).json({ message: "error occur" });
       }
     };
-    generateAccountDetail();
+    myFunc = generateAccountDetail;
+    myFunc();
   } else if (Number(message) === 2 && step === 2) {
-    response = "Your request has successfully rejected";
     step = 1;
-    return res.status(200).json({ message: response, status: "success" });
+    return res
+      .status(200)
+      .json({ message: "Your request has rejected successfully" });
   } else if (Number(message) === 1 && step === 3) {
     try {
       const verify_payment = await axios.post(
@@ -81,19 +76,24 @@ exports.PayMe = async (req, res, next) => {
         }
       );
       if (verify_payment.data.status) {
-        response = "Thank you, we have received your payment";
         step = 1;
-        return res.status(200).json({ message: response, status: "success" });
+        return res
+          .status(200)
+          .json({ message: "Thank you, we have received your payment" });
       } else if (!verify_payment.data.status) {
-        response = "We have not received your payment";
-        this.generateAccountDetail();
-        return res.status(200).json({ message: response, status: "success" });
+        myFunc();
+        return res
+          .status(200)
+          .json({ message: "We have not received your payment,try again" });
+      } else {
+        return res.status(500).json({ message: "an error occur" });
       }
     } catch (error) {
-      return res.status(500).json({ message: error, status: "failed" });
+      return res.status(500).json({ message: "error occur" });
     }
   } else {
-    response = "Invalid value,kindly enter correct value";
-    return res.status(500).json({ message: response, status: "failed" });
+    return res
+      .status(500)
+      .json({ message: "Invalid value,kindly enter correct value" });
   }
 };
